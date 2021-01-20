5425797471451e26029a3b0b32cbe36b73436baf
Angsuman Roy <angsuman1010@gmail.com>
2021-01-20 08:32:37 +0530

    Add project files.

---
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HandleError(View="SOError",ExceptionType =typeof(StackOverflowException))]
+        public ActionResult Index()
+        {
+            //throw new StackOverflowException();
+            return View();
+        }
+        [ActionName("Angsuman")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble? Contact US.";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Thanks!! We will get back to you.";
+            return View();
+        }
+        
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            string serial = "ASPNETMVCV5ATM";
+            if(letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            else
+            {
+                //return Content(serial);
+                //return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+                return RedirectToAction("Index");
+            }        
+        }
+    }
+}
